f401c41ae12db569deeb0c9c060efaa9cbc85ce1
Sritiman Adak <sritiman.adak@Sritimans-MacBook-Pro.local>
2022-07-16 21:44:43 +0530

    Initiated todo app project

---
+++ src/test/java/com/chaosmonkey/todoapp/TodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaosmonkey.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
